99233fa86f412a82ec9f97a80924d07e8f129efa
ANUJ KUMAR <Anujpatiyal@live.in>
2025-01-29 22:22:23 +0530

    added ChromeActionsTest java file

---
+++ src/test/java/org/ap/qa/selenium/ChromeActionsTest.java
@@ -0,0 +1,26 @@
+package org.ap.qa.selenium;
+
+import org.testng.annotations.Test;
+
+public class ChromeActionsTest {
+
+    @Test(priority = 1)
+    public void testOpenChromeBrowser() {
+        ChromeActions.openChromeBrowser();
+    }
+
+    @Test(dependsOnMethods = "testOpenChromeBrowser", priority = 2)
+    public void testMaximizeBrowserWindow() {
+        ChromeActions.maximizeBrowserWindow();
+    }
+
+    @Test(dependsOnMethods = "testMaximizeBrowserWindow", priority = 3)
+    public void testNavigateToURL() {
+        ChromeActions.navigateToURL("https://www.google.com");
+    }
+
+    @Test(dependsOnMethods = "testOpenChromeBrowser" , priority = 4)
+    public void testCloseChromeBrowser() {
+        ChromeActions.closeChromeBrowser();
+    }
+}
